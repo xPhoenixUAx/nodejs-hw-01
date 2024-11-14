12f5952d075545b246955ff29d4c85e37546498f
xPhoenixUAx <pavloff12@gmail.com>
2024-11-14 19:28:39 +0200

    +

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,16 @@
+import fs from 'fs/promises';
+import path from 'path';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const filePath = path.resolve(PATH_DB);
+
+    const data = await fs.readFile(filePath, 'utf-8');
+
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    return null;
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const writeContacts = async (updatedContacts) => {};
+import fs from 'fs/promises';
+import path from 'path';
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const filePath = path.resolve(PATH_DB);
+    const data = await fs.writeFile(filePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+    return null;
+  }
+};
